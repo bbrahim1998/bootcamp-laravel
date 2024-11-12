ddcbdb21e13bd68a4f5a8c559a58b6d060d6055d
bbrahim1998 <brahim98.taza98@gmail.com>
2024-11-12 11:19:45 +0100

    aquí creamos el trait y lo configuramos

---
+++ bootcamp/app/Trais/HasSlug.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Trais;
+
+use function DI\string;
+
+trait HasSlug
+{
+    //
+
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+
+//escucha evento de eloquent al crearse cosas en la tabla
+    public static function bootHasSlug(): void
+    {
+        static::saving(function ($model){
+            $model->slug =Str::slug($model->name);
+        });
+    }
+}
