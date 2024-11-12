3d64cc7549bdc973bd4f1bebccc8ff56d301649e
bbrahim1998 <brahim98.taza98@gmail.com>
2024-11-12 12:44:55 +0100

    modifico los arhcivos de los modelos para castear las variables y para proteger nuestra app de la asignación masiva de variables

--- bootcamp/app/Models/Category.php
+++ bootcamp/app/Models/Category.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use App\Traits\HasSlug;
+use App\Services\UploadService;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-    //
+    use HasSlug;
+
+    //proteger app ante asignación masiva de laravel
+    protected $filable = [
+        "name",
+        "slug",
+        "description",
+        "image",
+    ];
+
+    public function wines(): HasMany
+    {
+        return $this->hasMany(Wine::class);
+    }
+
+    public function imageUrl(): Attribute
+    {
+        return Attribute::make(
+            get: fn () =>UploadService::url($this->image),
+        );
+    }
+
 }
--- bootcamp/app/Traits/HasSlug.php
+++ bootcamp/app/Traits/HasSlug.php
@@ -17,7 +17,7 @@ trait HasSlug
     public static function bootHasSlug(): void
     {
         static::saving(function ($model){
-            $model->slug =Str::slug($model->name);
+            $model->slug = Str::slug($model->name);
         });
     }
 }
